8be4e50c1484372420e5b140ba8af83247f4a6bb
Michaël Zasso <mic.besace@gmail.com>
2015-08-25 10:43:49 +0200

    add catch method to message instance

--- src/webview/Message.js
+++ src/webview/Message.js
@@ -1,19 +1,25 @@
 'use strict';
 
 const EventEmitter = require('events').EventEmitter;
+const promise = Symbol();
 
 class Message extends EventEmitter {
     constructor(id, data) {
         super();
         this.id = id;
         this.data = data;
-        let prom = new Promise((resolve, reject) => {
+        this[promise] = new Promise((resolve, reject) => {
             this._resolve = resolve;
             this._reject = reject;
         });
-        this.then = function (onResolve, onReject) {
-            return prom.then(onResolve, onReject);
-        };
+    },
+
+    then(onResolve, onReject) {
+        return this[promise].then(onResolve, onReject);
+    }
+
+    catch(onReject) {
+        return this[promise].catch(onReject);
     }
 }
 
